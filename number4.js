8d03533970d37d6d707ac55b140c939b49e50909
mohamilin <amilinmoh99@gmail.com>
2020-08-17 21:01:51 +0700

    solved

--- number4.js
+++ number4.js
@@ -9,4 +9,19 @@
 */
 
 //SOLVE
-
+switch (prompt("Pilih Karakter Anda !")) {
+  case "UP":
+    alert("Karakter berjalan keatas");
+    break;
+  case "RIGHT":
+    alert("Karakter berjalan kekanan");
+    break;
+  case "BOTTOM":
+    alert("Karakter berjalan kebawah");
+    break;
+  case "LEFT":
+    alert("Karakter berjalan kekiri");
+    break;
+  default:
+    alert("Karakter tidak terdeteksi");
+}
